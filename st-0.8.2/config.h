9a3d4a8f46c8283e6af65311780afd7f849a1ef4
Monody <2304925521@qq.com>
2021-06-07 20:42:01 +0800

    Update config.h

--- st-0.8.2/config.h
+++ st-0.8.2/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Courier New:pixelsize=14:antialias=true:autohint=true";
+static char *font = "SauceCodePro Nerd Font Mono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
